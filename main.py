123765de9e5725f9d83d32fd357d3b46e7b48c23
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2024-01-18 17:31:30 -0300

    Refactor code to open multiple tabs and verify participants in the room

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ async def criar_sala(num_participantes:int = 1):
     
     host_da_sala = webdriver.Chrome(service=service)
 
-
+    tabs = [host_da_sala.current_window_handle]
     
     host_da_sala.get(config.URL)
 
@@ -36,7 +36,6 @@ async def criar_sala(num_participantes:int = 1):
     wait.until(EC.presence_of_element_located((By.ID, 'input-nick'))) \
         .send_keys("Host")
 
-    print("Aguardando botão de criar sala e entrar ficar disponível")
     assert wait.until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Criar Sala e Entrar")]'))) \
         .is_enabled()
     print("Teste passou: botão de criar sala e entrar está disponível\n")
@@ -47,26 +46,29 @@ async def criar_sala(num_participantes:int = 1):
     input_com_link = copiar_parent.find_element(By.TAG_NAME, 'input')
     link = input_com_link.get_attribute('value')
 
-    participantes_thread = []
     participantes_nomes = ["Host"]
 
     for i in range(num_participantes):
         id = str(uuid.uuid4())[:6]
         participantes_nomes.append(f"P-{id}")
-        print(participantes_nomes)
-        thread = Thread(target=entrar_na_sala, args=(link,id,), name=f'Participante {i + 1}')
-        participantes_thread.append(thread)
-        thread.start()
+        host_da_sala.switch_to.new_window('tab')
+        host_da_sala.get(link)
+        tabs.append(host_da_sala.current_window_handle)
+        entrar_na_sala(host_da_sala, id)
 
-    alterar_thread = Thread(target=alterar_papel, args=(host_da_sala, len(participantes_nomes)), name='Alterar Papel')
-    alterar_thread.start()
+    host_da_sala.switch_to.window(tabs[0])
+    alterar_papel(host_da_sala, len(participantes_nomes))
+    for  tab in tabs:
+        host_da_sala.switch_to.window(tab)
+        verificar_se_esta_na_sala(host_da_sala)
 
-    for thread in participantes_thread:
-        thread.join()
-
-    time.sleep(5)
+    print("Teste passou: todos os participantes estão na sala\n")
     host_da_sala.quit()
-    time.sleep(5)
+    return
+
+def verificar_se_esta_na_sala(driver:webdriver.Chrome):
+    wait = WebDriverWait(driver, 10)
+    assert wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'Tabuleiro'))).is_displayed()
     return
 
 def alterar_papel(driver:webdriver.Chrome, num_participantes:int = 1):
@@ -80,17 +82,14 @@ def alterar_papel(driver:webdriver.Chrome, num_participantes:int = 1):
         .click()
     return
 
-def entrar_na_sala(link, id):
+def entrar_na_sala(driver, id):
 
-    print(f"Usuario P-{id} entrando na sala {link}")
-    participante_da_sala = webdriver.Chrome(service=service)
-    participante_da_sala.get(link)
-    wait = WebDriverWait(participante_da_sala, 10)
+
+    wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.ID, 'input-nick'))) \
         .send_keys(f"P-{id}")
     wait.until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Entrar na Sala")]'))) \
         .click()
-    participante_da_sala.quit()
     return
 
 if __name__ == '__main__':
